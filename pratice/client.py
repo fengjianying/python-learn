623dfbecb86934b7f8aefbfc3e7d241a71772270
fengjianying <872808932@qq.com>
2018-09-21 10:24:17 +0800

    实践代码提交

--- pratice/client.py
+++ pratice/client.py
@@ -1,13 +1,20 @@
-import sys
 import pygame
+from setting import Settings
 def run_game():
+    #初始化屏幕对象
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    client_config = Settings()
+    screen = pygame.display.set_mode((client_config.width, client_config.height))
     pygame.display.set_caption("test client")
+    #开启主循环
     while True:
+        #填充背景色
+        screen.fill(client_config.bgc);
+        #监听键盘和鼠标事件
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        #让最近绘制的屏幕可见
         pygame.display.flip()
         
 run_game()
---
+++ pratice/setting.py
@@ -0,0 +1,5 @@
+class Settings():
+    def __init__(self):
+        self.width = 1200
+        self.height = 800
+        self.bgc = (230,230,230)
